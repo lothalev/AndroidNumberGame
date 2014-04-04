2f8320d92ca054e5c85d64352859b924657da56d
lothalev <lothalev@gmail.com>
2014-04-04 12:52:34 +0200

    count digits for achievements

--- src/com/lothalev/guessthenumber/NumberGame.java
+++ src/com/lothalev/guessthenumber/NumberGame.java
@@ -3,13 +3,41 @@ package com.lothalev.guessthenumber;
 import java.util.Random;
 
 public class NumberGame {
-	public int currentMin, currentMax;
+	public class Score {
+		private int level, tries, digitcounts[];
+		
+		public Score(int level, int tries, int[] digitcounts){
+			this.level=level;
+			this.tries=tries;
+			this.digitcounts=digitcounts;
+		}
+		public int getLevel(){
+			return level;
+		}
+		
+		public int getTries(){
+			return tries;
+		}
+		
+		public int[] getCounts(){
+			return digitcounts;
+		}
+	}
+
 	private int numToGuess;
+	private int level;
 	public int tries;
+	public int currentMin, currentMax;
+	private int digitcounts[];
 	
 	public NumberGame(int level){
+		this.level=level;
 		currentMax=getMaxFromLevel(level);
 		currentMin=0;
+		for (int i=0;i<5;i++) {
+			digitcounts[i]=0;
+		}
+		
 		tries=0;
 		Random r = new Random();
 		numToGuess = r.nextInt(currentMax)+1;
@@ -17,6 +45,8 @@ public class NumberGame {
 	
 	public boolean Guess(int guess){
 		tries++;
+		digitcounts[(Integer.toString(guess).length())]++;
+			
 		if (guess == numToGuess) return true;
 		if (guess>numToGuess && guess<currentMax)
 			currentMax=guess;
@@ -41,5 +71,9 @@ public class NumberGame {
 			return 10000;
 		}
 		return -1;
+	}
+	
+	public Score getHighScore() {
+		return new Score(level, tries, digitcounts);
 	}
 }
